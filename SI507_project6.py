c7e6bafb0200a1bfe4c48f46ee17d871d9704281
ltsheridan <31678807+ltsheridan@users.noreply.github.com>
2017-11-27 23:03:36 -0500

    commiting parts 1 and 2

--- SI507_project6.py
+++ SI507_project6.py
@@ -6,7 +6,7 @@ from config import *
 import requests
 import sys
 import json
-import csv
+from csv import DictReader
 
 # Write code / functions to set up database connection and cursor here.
 db_connection, db_cursor = None, None
@@ -29,7 +29,7 @@ def get_connection_and_cursor():
 
     return db_connection, db_cursor
 
-db_connection, db_cursor = get_connection_and_cursor()
+#db_connection, db_cursor = get_connection_and_cursor()
 
 # Write code / functions to create tables with the columns you want and all database setup here.
 def setup_database():
@@ -45,61 +45,65 @@ def setup_database():
 
 # Write code / functions to deal with CSV files and insert data into the database here.
 
-arkansas_data=open('arkansas.csv', 'r')
-california_data=open('california.csv','r')
-michigan_data=open('michigan.csv','r')
-ak_reader=csv.DictReader(arkansas_data)
-ca_reader=csv.DictReader(california_data)
-mi_reader=csv.DictReader(michigan_data)
+# arkansas_data=open('arkansas.csv', 'r')
+# california_data=open('california.csv','r')
+# michigan_data=open('michigan.csv','r')
+# ak_reader=csv.DictReader(arkansas_data)
+# ca_reader=csv.DictReader(california_data)
+# mi_reader=csv.DictReader(michigan_data)
 
 
 
+def insert_site(name, type, location, description, state_id):
+    sql = "INSERT INTO Sites(Name, Type, Location, Description, state_id) VALUES(%s, %s, %s, %s, %s)"
+    db_cursor.execute(sql,(name, type, location, description, state_id))
+    db_connection.commit()
 
+#
+def insert_states(state_name, csv):
+    sql = "INSERT INTO States(Name) VALUES(%s) RETURNING id"
+    db_cursor.execute(sql,(state_name,))
+    state_id = db_cursor.fetchone()['id']
+    state_data = open(csv,'r')
+    reader = DictReader(state_data)
 
-# def insert_site(conn, cur, name, type, location, description, state_id):
-#     #results=cur.fetchone()[0]
-#     sql = "INSERT INTO Sites(Name, Type, Location, Description) VALUES(%s, %s, %s, %s)"
-#     cur.execute(sql,(park_name, park_type, park_location, description))
-#     conn.commit()
+    for site in reader:
+        name = site['NAME']
+        types = site['TYPE']
+        location = site['LOCATION']
+        description = site['DESCRIPTION']
+        insert_site(name, types, location, description, state_id)
 
-
-def insert_states(conn, cur, state_name):
-
-    sql = "INSERT INTO States(Name) VALUES(%s)"
-
-    cur.execute(sql,(state_name,))
-    id = cur.fetchone()[0]
-    conn.commit()
+    db_connection.commit()
     return id
-get_connection_and_cursor()
-setup_database()
 
-state_id_mi = insert_states(db_connection, db_cursor, 'michigan')
 
-for site in mi_reader:
-    print(site)
-    name = site['NAME']
-    types = site['TYPE']
-    location = site['LOCATION']
-    description = site['DESCRIPTION']
-    insert_site(db_connection, db_cursor, name, types, location, description, state_id_mi)
-    print(site)
+
 
 # Make sure to commit your database changes with .commit() on the database connection.
 
 
 
 # Write code to be invoked here (e.g. invoking any functions you wrote above)
+get_connection_and_cursor()
+setup_database()
+insert_states('michigan','michigan.csv')
+insert_states('arkansas','arkansas.csv')
+insert_states('california','california.csv')
 
-for site in sites_reader:
-    print(type(site))
-print(results[0])
 
 # Write code to make queries and save data in variables here.
 
+def execute(query, numer_of_results=1):
+    db_cursor.execute(query)
+    results = db_cursor.fetchall()
 
 
-
+all_locations=execute("select location from Sites")
+beautiful_sites=execute("""select Name from Sites where Description ilike '%beautiful%'""")
+natl_lakeshores=execute("select count(Type) from Sites where Type='National Lakeshore'")
+michigan_names=execute("select Sites.Name from Sites INNER JOIN States ON (States.ID=Sites.ID)")
+total_number_arkansas=("select count(Sites.ID) from Sites INNER JOIN States ON (States.ID=Sites.ID) where Name ilike '%Arkansas%'")
 
 
 # We have not provided any tests, but you could write your own in this file or another file, if you want.
